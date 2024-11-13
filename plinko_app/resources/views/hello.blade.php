8287047323b0b142e0974fe5f4dca63c091d8f55
AttilaKorozs <korozs.attila92@gmail.com>
2024-11-13 20:04:19 +0000

    Főoldal szövegének módosítása

--- plinko_app/resources/views/hello.blade.php
+++ plinko_app/resources/views/hello.blade.php
@@ -7,9 +7,9 @@
 <div class="card mb-3">
     <div class="card-body" style="text-align: center;">
         <img src="kepek/sor.jpg" class="rounded img-fluid" alt="Beer" style="max-width: 80vmin">
-        <p class="card-text">Hello, ez itt a főoldal, csak sajna még nincs kész.</p>
+        <p class="card-text">Hello, ez itt egy Plinko játék ahol akár igazi pénzt is nyerhetsz.</p>
         <p class="card-text">
-            <small class="text-body-secondary">Ha szeretnéd hogy hamarabb elkészüljön, segíts.</small>
+            <small class="text-body-secondary">Csak 3 éven felülieknek.</small>
         </p>
     </div>
 </div>
