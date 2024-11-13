1e2d37074a173eb96ec1f1d59a1026399a5b26fd
AttilaKorozs <korozs.attila92@gmail.com>
2024-11-13 20:04:45 +0000

    hiba oldal elkészítése

--- plinko_app/resources/views/errors/404.blade.php
+++ plinko_app/resources/views/errors/404.blade.php
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="UTF-8" />
-    <title>Atix Söri</title>
-    <meta name="description" content="Nem létező oldal.">
-    <meta name="author" content="Atix">
-    <link rel="icon" type="image/png" href="{{ asset('kepek/beer.png') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
-<body class="mx-4 bg-light">
+@extends('skeleton')
+
+@section('title', 'Főoldal')
+
+@section('content')
+
+
     <div class="card mb-3">
         <div class="card-body" style="text-align: center;">
             <img src="{{ asset('kepek/404beer.jpg') }}" class="rounded img-fluid" alt="404" style="max-width: 80vmin">
@@ -20,5 +14,5 @@
                 </small></p>
         </div>
     </div>
-</body>
-</html>
+
+@endsection
