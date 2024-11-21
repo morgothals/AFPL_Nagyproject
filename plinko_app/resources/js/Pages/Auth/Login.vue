4a02a3b3c6729ea6091e5e44eaf9680755c60043
GaluskaGergely <galuskagergely@gmail.com>
2024-11-21 21:16:26 +0100

    Login-Register magyarítás

--- plinko_app/resources/js/Pages/Auth/Login.vue
+++ plinko_app/resources/js/Pages/Auth/Login.vue
@@ -30,8 +30,9 @@ const submit = () => {
 </script>
 
 <template>
+
     <GuestLayout>
-        <Head title="Log in" />
+        <Head title="Bejelentkezés" />
 
         <div v-if="status" class="mb-4 text-sm font-medium text-green-600">
             {{ status }}
@@ -39,7 +40,7 @@ const submit = () => {
 
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="email" value="Email" />
+                <InputLabel for="email" value="E-mail" />
 
                 <TextInput
                     id="email"
@@ -55,7 +56,7 @@ const submit = () => {
             </div>
 
             <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+                <InputLabel for="password" value="Jelszó" />
 
                 <TextInput
                     id="password"
@@ -73,7 +74,7 @@ const submit = () => {
                 <label class="flex items-center">
                     <Checkbox name="remember" v-model:checked="form.remember" />
                     <span class="ms-2 text-sm text-gray-600 dark:text-gray-400"
-                        >Remember me</span
+                        >Emlékezz rám</span
                     >
                 </label>
             </div>
@@ -84,7 +85,7 @@ const submit = () => {
                     :href="route('password.request')"
                     class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
                 >
-                    Forgot your password?
+                    Elfelejtett jelszó?
                 </Link>
 
                 <PrimaryButton
@@ -92,7 +93,7 @@ const submit = () => {
                     :class="{ 'opacity-25': form.processing }"
                     :disabled="form.processing"
                 >
-                    Log in
+                    Bejelentkezés
                 </PrimaryButton>
             </div>
         </form>
